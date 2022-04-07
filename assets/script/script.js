0ddde9409e582739839b7dd765970deb43fda717
Sophia C <95388964+Sophtron5000@user.noreply.github.com>
2022-04-06 23:26:44 -0700

    final save of the night

--- assets/script/script.js
+++ assets/script/script.js
@@ -10,6 +10,8 @@ setInterval(() => {
 }, 1000);
 // WHEN I scroll down
 // THEN I am presented with timeblocks for standard business hours
+
+
 // WHEN I view the timeblocks for that day
 // THEN each timeblock is color coded to indicate whether it is in the past, present, or future
 // WHEN I click into a timeblock
